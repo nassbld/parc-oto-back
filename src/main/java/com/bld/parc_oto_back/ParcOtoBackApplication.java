b7357ef024f1d464860994dee0f0353634a7944d
NassBld <nassimbld@gmail.com>
2025-01-02 00:11:12 +0100

    First commit

---
+++ src/main/java/com/bld/parc_oto_back/ParcOtoBackApplication.java
@@ -0,0 +1,13 @@
+package com.bld.parc_oto_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcOtoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcOtoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bld/parc_oto_back/ParcOtoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bld.parc_oto_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcOtoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
